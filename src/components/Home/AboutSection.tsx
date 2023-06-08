043f837a38f16180344cd6a7ccad3c7c2aeaaadc
rabbitSheriff <cohenc700@gmail.com>
2023-06-07 19:00:06 -0500

    🛠️ FEAT: Add in home page the `AboutSection.tsx`
    *Add images for the new section.*
    
    *Refactor the `index.astro`.*
    
    *Add 2 svg's component for customization.*

--- src/components/Home/AboutSection.tsx
+++ src/components/Home/AboutSection.tsx
@@ -1,7 +1,49 @@
+import CoffeeMachine from "../CoffeeMachine";
+import LatteCoffeeIcon from "../LatteCoffeeIcon";
+import PrimaryButton from "../PrimaryButton";
+
 function AboutSection() {
   return (
     <section className="w-full">
-      <div className="mx-auto aspect-video w-full max-w-[1728px] bg-red-400"></div>
+      <div className="w-full max-w-[1728px] sm:py-24 xl:mx-auto xl:flex xl:flex-row-reverse xl:items-center xl:justify-center xl:gap-24 xl:py-28">
+        <div className="sm:flex sm:flex-row-reverse sm:gap-16 sm:px-16 sm:pb-24 xl:max-w-[600px] xl:flex-col-reverse xl:p-0">
+          <div className="mx-auto flex items-end justify-evenly px-6 py-8 sm:w-1/3 sm:justify-between sm:p-0 xl:w-full xl:justify-start">
+            <CoffeeMachine extendClass="w-[120px] h-[200px] xl:w-[235px] h-[360px] sm:mr-4 xl:mr-24" />
+            <LatteCoffeeIcon extendClass="w-24 h-24 xl:w-[180px] xl:h-[180px]" />
+          </div>
+          <div className="flex flex-col items-center px-6 py-8 sm:w-2/3 sm:p-0 xl:w-full xl:items-start">
+            <h2 className="w-full text-3xl font-bold tracking-tight text-brown-800">
+              We are committed to making the best coffee for you.
+            </h2>
+            <p className="mb-10 mt-4 leading-7 text-brown-700">
+              We believe that coffee is not just a drink, but an experience. That's why we
+              use only the best coffee beans and ingredients to create our unique and
+              delicious coffee blends.
+            </p>
+            <PrimaryButton
+              path="/about-us"
+              name="About us"
+              buttonClass="px-5 py-2 sm:px-6 sm:py-3 sm:text-2xl text-lg"
+              ariaLabel="About us button"
+            />
+          </div>
+        </div>
+        <div className="py-8 sm:px-16 sm:py-0 xl:p-0">
+          <picture>
+            <source
+              srcSet="/assets/images/home-about/table-with-coffees-large.jpg 1x, /assets/images/home-about/table-with-coffees-large-2x.jpg 2x"
+              media="(min-width:640px)"
+            />
+            <img
+              className="aspect-[4/5] w-full sm:mx-auto sm:max-w-[600px] xl:m-0 "
+              src="/assets/images/home-about/table-with-coffees-small.jpg"
+              srcSet="/assets/images/home-about/table-with-coffees-small.jpg 1x, /assets/images/home-about/table-with-coffees-small-2x.jpg 2x"
+              alt="Table with coffes"
+              loading="lazy"
+            />
+          </picture>
+        </div>
+      </div>
     </section>
   );
 }
